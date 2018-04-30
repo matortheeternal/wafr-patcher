4597eebd9d9bb9bcf62b9611c6f658f650ad9d7c
Colin <why101@msn.com>
2018-04-30 10:39:02 -0700

    v1.0 release

---
+++ dist/index.js
@@ -0,0 +1,98 @@
+/* global ngapp, xelib, registerPatcher */
+
+const targetMaterials = ['Elven', 'Dwarven', 'Orcish'];
+
+const levelShifts = {
+    'Elven': -7,
+    'Dwarven': -6,
+    'Orcish': 13
+};
+
+let fixLevel = function(entry, material, name, log) {
+    let level = xelib.GetUIntValue(entry, 'LVLO\\Level'),
+        shift = levelShifts[material];
+    if (level === 25) shift--;
+    if (level === 11) shift++;
+    let newLevel = Math.max(level + shift, 1);
+    log(`  Changing level for entry ${name} from ${level} to ${newLevel}`);
+    xelib.SetUIntValue(entry, 'LVLO\\Level', newLevel);
+};
+
+let getMaterialEntry = function(equipment, name) {
+    for (let material of Object.keys(equipment)) {
+        let entry = equipment[material].find(e => e === name);
+        if (entry) return material;
+    }
+};
+
+let getMaterial = function(rec) {
+    if (!xelib.HasElement(rec, 'KWDA') ||
+        !xelib.HasElement(rec, 'EITM')) return;
+    let keywords = xelib.GetElements(rec, 'KWDA').map(e => xelib.GetValue(e));
+    for (let keyword of keywords) {
+        let match = keyword.match(/(Armor|Weap)Material(\S+)/);
+        if (match) return match[2];
+    }
+};
+
+let addEquipment = function(equipment, rec) {
+    let material = getMaterial(rec);
+    if (!targetMaterials.includes(material)) return;
+    if (!equipment.hasOwnProperty(material)) equipment[material] = [];
+    equipment[material].push(xelib.LongName(rec));
+};
+
+let patchLeveledItem = function(patch, record, helpers, locals) {
+    if (!xelib.HasElement(record, 'Leveled List Entries')) return;
+    record = xelib.CopyElement(record, patch);
+    helpers.logMessage(`Patching ${xelib.Name(record)}`);
+    let entries = xelib.GetElements(record, 'Leveled List Entries');
+    entries.forEach(entry => {
+        let item = xelib.GetValue(entry, 'LVLO\\Reference'),
+            material = getMaterialEntry(locals.equipment, item);
+        if (!material) return;
+        fixLevel(entry, material, item, helpers.logMessage);
+    });
+}
+
+registerPatcher({
+    info: info,
+    gameModes: [xelib.gmTES5, xelib.gmSSE],
+    settings: {
+        label: 'WAFR Patcher',
+        templateUrl: `${patcherPath}/partials/settings.html`,
+        defaultSettings: {}
+    },
+    requiredFiles: [],
+    getFilesToPatch: function(filenames) {
+        let excludedFiles = [
+            'Skyrim.esm', 
+            'Update.esm', 
+            'Dawnguard.esm', 
+            'HearthFires.esm', 
+            'Dragonborn.esm',
+            'Weapon AF.esp',
+            'Weapon & Armor Fixes_Remade.esp'
+        ];
+        for (let i = filenames.length - 1; i >= 0; i--) {
+            if (!excludedFiles.includes(filenames[i])) continue;
+            filenames.splice(i, 1);
+        }
+        return filenames;
+    },
+    execute: {
+        initialize: function(patch, helpers, settings, locals) {
+            let equipment = xelib.GetRecords(0, 'WEAP,ARMO');
+            locals.equipment = {};
+            equipment.forEach(function(rec) {
+                if (!xelib.IsMaster(rec)) return;
+                addEquipment(locals.equipment, rec);
+            });
+            let leveledItems = helpers.loadRecords('LVLI', true);
+            leveledItems.forEach(record => {
+                patchLeveledItem(patch, record, helpers, locals);
+            })
+        },
+        process: []
+    }
+});
